a942969bcffc11ac81b77d830dd39f466fc2bb63
Shravan Sundaram <shravan,sundaram@gmail.com>
2018-03-26 20:05:28 -0400

    WebBlogAppComplete
    Completion of the Web Blog App

--- full-stack/web_blog/src/app.py
+++ full-stack/web_blog/src/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, make_response
 
 from models.blog import Blog
+from models.post import Post
 from src.common.database import Database
 from src.models.user import User
 
@@ -8,9 +9,9 @@ app = Flask(__name__)
 app.secret_key = "shravan"
 
 
-@app.route('/')
-def home_template():
-    return home_template('home.html')
+# @app.route('/')
+# def home_template():
+#     return home_template('home.html')
 
 
 @app.route('/login')
@@ -63,12 +64,42 @@ def user_blogs(user_id=None):
     return render_template("user_blogs.html", blogs=blogs, email=user.email)
 
 
+@app.route("/blogs/new", methods=["POST", "GET"])
+def create_new_blog():
+    if request.method == "GET":
+        return render_template("new_blog.html")
+    else:
+        title = request.form['title']
+        description = request.form['description']
+        user = User.get_by_email(session['email'])
+
+        new_blog = Blog(user.email, title, description, user._id)
+        new_blog.save_to_mongo()
+
+        return make_response(user_blogs(user._id))
+
+
 @app.route('/posts/<string:blog_id>')
 def blog_posts(blog_id):
     blog = Blog.from_mongo(blog_id)
     posts = blog.get_posts()
 
-    return render_template("posts.html", posts=posts, blog_title=blog.title)
+    return render_template("posts.html", posts=posts, blog_title=blog.title, blog_id=blog_id)
+
+
+@app.route("/posts/new/<string:blog_id>", methods=["POST", "GET"])
+def create_new_post(blog_id):
+    if request.method == "GET":
+        return render_template("new_post.html", blog_id=blog_id)
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post = Post(blog_id, title, content, user.email )
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))
 
 
 if __name__ == '__main__':
