eb39069f6a1f846ed775724d6aad8c76e53a560d
Shravan Sundaram <shravan,sundaram@gmail.com>
2018-03-31 23:07:38 -0400

    Adding new project. Price of item

---
+++ full-stack/Price-of-Item-Web/src/models/users/user.py
@@ -0,0 +1,11 @@
+import uuid
+
+
+class User(object):
+    def __init__(self, email, password, _id = None):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
