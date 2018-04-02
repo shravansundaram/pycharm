a189b07db96b25424bf9a6016924b064033e3924
Shravan Sundaram <shravan,sundaram@gmail.com>
2018-04-02 16:52:40 -0400

    Adding new project. Price of item. More files

--- full-stack/Price-of-Item-Web/src/models/users/user.py
+++ full-stack/Price-of-Item-Web/src/models/users/user.py
@@ -1,4 +1,7 @@
 import uuid
+from src.common.database import Database
+from src.common.utils import Utils
+import src.models.users.errors as UserErrors
 
 
 class User(object):
@@ -8,4 +11,24 @@ class User(object):
         self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
-        return "<User {}>".format(self.email)
+        return '<User {}>'.format(self.email)
+
+    @staticmethod
+    def is_valid_login(email, password):
+        """
+        This method verifies that an e-mail/password combo (sent from the website) is valid or not
+        Checks that the email exists and the password specified is correct
+        :param email: The User's e-mail
+        :param password: A sha512 hashed password
+        :return: True if valid, else False
+        """
+
+        user_data = Database.find_one(collection="users",
+                                      query={"email": email}
+                                      )
+        if user_data is None:
+            raise UserErrors.UserNotExistError("Your User does not Exist")
+        if not Utils.check_hashed_password(password, user_data['password']):
+            raise UserErrors.IncorrectPasswordError("Your Password is incorrect")
+
+        return True
--- full-stack/Price-of-Item-Web/src/models/users/views.py
+++ full-stack/Price-of-Item-Web/src/models/users/views.py
@@ -1,11 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request, session, url_for, render_template
+from werkzeug.utils import redirect
+
+from src.models.users.user import User
 
 user_blueprint = Blueprint('users', __name__)
 
 
-@user_blueprint.route("/login")
+@user_blueprint.route("/login", methods=["GET","POST"])
 def login_user():
-    pass
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["hashed"]
+
+        if User.is_valid_login(email, password):
+            session["email"] = email
+            return redirect(url_for(".user.alerts"))
+
+    return render_template("users/login.html")
 
 
 @user_blueprint.route("/register")
@@ -15,7 +26,7 @@ def register_user():
 
 @user_blueprint.route("/alerts")
 def user_alerts():
-    pass
+    return "This is the Alerts Page"
 
 
 @user_blueprint.route("/logout")
@@ -23,6 +34,6 @@ def logout_user():
     pass
 
 
-@user_blueprint.route("/check_alerts/<string:user_id")
+@user_blueprint.route("/check_alerts/<string:user_id>")
 def check_user_alert(user_id):
     pass
